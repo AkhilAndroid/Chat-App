fe17b94daedfa2cb1795d579cc19076b76cbf27b
AkhilAndroid <74596852+AkhilAndroid@users.noreply.github.com>
2020-11-18 18:46:31 +0530

    Commit to master

--- app/src/main/java/com/example/chatplease/Login.java
+++ app/src/main/java/com/example/chatplease/Login.java
@@ -1,12 +1,8 @@
 package com.example.chatplease;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.LoginFilter;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
@@ -14,12 +10,13 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.dynamite.DynamiteModule;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -89,6 +86,7 @@ public class Login extends AppCompatActivity {
                 }
             }
         });
+
 
     }
     private void Initializingfields()
